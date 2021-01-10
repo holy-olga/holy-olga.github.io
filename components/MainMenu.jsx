9dc4cc9125107eb08ed4e8715b493b847b294ef8
David Morasz <david.morasz@meso.design>
2021-01-10 02:52:13 +0100

    ^ menu items are boxes now

--- components/MainMenu.jsx
+++ components/MainMenu.jsx
@@ -8,22 +8,22 @@ export default class MainMenu extends React.Component {
             <div className="cover">
             </div>
             <div className="flexing">
-                <Gh2 onlyhover glitchtype="1" className="menuItem">
+                <Gh2 onlyhover glitchtype="2" className="menuItem">
                     <Link to="/c/log">Art Projects</Link>
                 </Gh2>
-                <Gh2 onlyhover glitchtype="1" className="menuItem">
+                <Gh2 onlyhover glitchtype="2" className="menuItem">
                     <Link to="/c/works">Exhibitions</Link>
                 </Gh2>
-                <Gh2 onlyhover glitchtype="1" className="menuItem">
+                <Gh2 onlyhover glitchtype="2" className="menuItem">
                     <Link to="/c/ware">Festivals</Link>
                 </Gh2>
-                <Gh2 onlyhover glitchtype="1" className="menuItem">
+                <Gh2 onlyhover glitchtype="2" className="menuItem">
                     <Link to="/c/ware">Issues</Link>
                 </Gh2>
-                <Gh2 onlyhover glitchtype="1" className="menuItem">
+                <Gh2 onlyhover glitchtype="2" className="menuItem">
                     <Link to="/c/ware">Applied Works</Link>
                 </Gh2>
-                <Gh2 onlyhover glitchtype="1" className="menuItem">
+                <Gh2 onlyhover glitchtype="2" className="menuItem">
                     <Link to="/c/about">About</Link>
                 </Gh2>
             </div>
